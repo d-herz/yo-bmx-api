2752f693262ff6526c4ac0e28bc696a2b9318e08
Daniel Hyres <danhyres@gmail.com>
2023-03-30 19:33:10 -0400

    Building out user schema

--- models/User.js
+++ models/User.js
@@ -4,18 +4,54 @@ const bcrypt = require("bcrypt");
 const UserSchema = new Schema({
   userName: {
     type: String,
+    required: true,
     unique: true,
   },
 
   email: {
     type: String,
+    required: true,
     unique: true,
-
   },
 
-  signUpDate: {
+  password: {
+    type: String,
+    required: true,
+  },
+
+  name: {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    }
+  },
+
+  created_at: {
     type: Date, 
     default: Date.now,
-  }
+  },
+
+  lastActive: {
+    type: Date,
+    default: new Date,
+  },
+  
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+
+  posts: {
+    // array of the users original posts
+  },
+
+  profilePic: {
+    // Do we need this
+  },
+
 
 })
